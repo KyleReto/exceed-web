d36cbb3c71f0d7d1aad585416da1f52de59ad1c9
KyleReto <40499747+KyleReto@users.noreply.github.com>
2022-06-15 01:58:18 -0400

    Finished gamestate, and added test data

--- views/scripts/engine/GameState.js
+++ views/scripts/engine/GameState.js
@@ -1,4 +1,70 @@
 class GameState{
-    constructor(){
+    constructor(p1, p2, otherCards){
+        this.p1 = p1;
+        this.p2 = p2;
+        this.otherCards = otherCards;
     }
+}
+
+class Player{
+    constructor(character, position, deck){
+        this.character = character;
+        this.life = 30;
+        this.position = position;
+        this.isExceed = false;
+        this.hasReshuffled = false;
+        this.sealed = [];
+        this.hand = [];
+        this.gauge = [];
+        this.deck = deck;
+        this.discard = [];
+    }
+}
+
+class Card{
+    constructor(name, mainText, type,
+             owner = null, secondText = '', mainCost = null, secondCost = null, stats = null){
+        this.name = name;
+        this.owner = owner;
+        this.mainText = mainText;
+        this.type = type;
+        this.secondText = secondText;
+        this.mainCost = mainCost;
+        this.secondCost = secondCost;
+        this.stats = stats;
+    }
+}
+
+class CardStats{
+    constructor(minRange = null, maxRange = null, power = null,
+             speed = null, armor = null, guard = null){
+        this.minRange = minRange;
+        this.maxRange = maxRange;
+        this.power = power;
+        this.speed = speed;
+        this.armor = armor;
+        this.guard = guard;
+    }
+}
+
+class Cost{
+    constructor(costValue, costType, transformationDiscount){
+        this.costValue = costValue;
+        this.costType = costType;
+        this.transformationDiscount = transformationDiscount;
+    }
+}
+
+const CardType = {
+    Character: 'Character',
+    Normal: 'Normal',
+    Special: 'Special',
+    Ultra: 'Ultra',
+    Astral: 'Astral',
+    Unique: 'Unique'
+}
+
+const CostType = {
+    Force: 'Force',
+    Gauge: 'Gauge'
 }
--- views/scripts/engine/main.js
+++ views/scripts/engine/main.js
@@ -1,4 +1,14 @@
 // On page load
 $(document).ready(function(){
-    console.log('test');
+    let testNormal = new Card('Dive', 'Before: Advance 3. If you moved past the opponent, their attacks do not hit you.',
+         CardType.Normal);
+    let testCharacter1 = new Card('Ryu', 'As an action, you may Move 1', CardType.Character);
+    let testCharacter2 = new Card('Ken', 'As an action, you may Close 1 and draw a card',
+         CardType.Character);
+    let testState = new GameState(
+        new Player(testCharacter1, 2, [testNormal, testNormal, testNormal]),
+        new Player(testCharacter2, 6, [testNormal, testNormal, testNormal]),
+        null
+    );
+    console.log(testState);
 });

7cd9debe50a3da2b99d5fbe2e7d8ef176ae682ec
KyleReto <40499747+KyleReto@users.noreply.github.com>
2022-06-15 23:58:26 -0400

    Added rudimentary renderer

---
+++ views/scripts/engine/Renderer.js
@@ -0,0 +1,17 @@
+class Renderer{
+
+    render(state){
+        $('#board').append('<span>' + state.p1.character.name + ' vs ' + 
+        state.p2.character.name +'</span>');
+        for (let i = 0; i < 9; i++){
+            let appendStr = '<div>' + i + ': ';
+            console.log(state.p1.position);
+            if (state.p1.position == i){
+                appendStr += state.p1.character.name;
+            } else if (state.p2.position == i){
+                appendStr += state.p2.character.name;
+            }
+            $('#board').append(appendStr + '</div>');
+        }
+    }
+}
